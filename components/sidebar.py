a70c262bfcd352f087cccca596694a9db4382d7b
André Freitas <dre.freitas23@gmail.com>
2022-11-17 21:21:33 -0300

    [fix] Sidebar callbacks

--- components/sidebar.py
+++ components/sidebar.py
@@ -170,7 +170,7 @@ layout = dbc.Col([
                     dbc.Label("Extras"),
                     dbc.Checklist(
                         options=[{"label": "Foi recebida", "value": 1},
-                                 {"label": "despesa Recorrente", "value": 2}],
+                                 {"label": "Despesa Recorrente", "value": 2}],
                         value=[1],
                         id="switches-input-despesa",
                         switch=True),
@@ -345,3 +345,104 @@ def salve_form_receita(n, descricao, valor, date, switches, categoria, dict_desp
 
     data_return = df_despesas.to_dict()
     return data_return
+
+# Add/Remove categoria despesa
+@app.callback(
+    [
+        Output("category-div-add-despesa", "children"),
+        Output("category-div-add-despesa", "style"),
+        Output("select_despesa", "options"),
+        Output('checklist-selected-style-despesa', 'options'),
+        Output('checklist-selected-style-despesa', 'value'),
+        Output('stored-cat-despesas', 'data')
+    ],
+
+    [
+        Input("add-category-despesa", "n_clicks"),
+        Input("remove-category-despesa", 'n_clicks')
+    ],
+
+    [
+        State("input-add-despesa", "value"),
+        State('checklist-selected-style-despesa', 'value'),
+        State('stored-cat-despesas', 'data')
+    ]
+)
+def add_category(n, n2, txt, check_delete, data):
+    cat_despesa = list(data["Categoria"].values())
+
+    txt1 = []
+    style1 = {}
+
+    if n:
+        if txt == "" or txt == None:
+            txt1 = "O campo de texto não pode estar vazio para o registro de uma nova categoria."
+            style1 = {'color': 'red', 'margin-top': '16px'}
+
+        else:
+            cat_despesa = cat_despesa + [txt] if txt not in cat_despesa else cat_despesa
+            txt1 = f'A categoria {txt} foi adicionada com sucesso!'
+            style1 = {'color': 'green', 'margin-top': '16px'}
+    
+    if n2:
+        if len(check_delete) > 0:
+            cat_despesa = [i for i in cat_despesa if i not in check_delete]  
+    
+    opt_despesa = [{"label": i, "value": i} for i in cat_despesa]
+    df_cat_despesa = pd.DataFrame(cat_despesa, columns=['Categoria'])
+    df_cat_despesa.to_csv("df_cat_despesa.csv")
+    data_return = df_cat_despesa.to_dict()
+
+    return [txt1, style1, opt_despesa, opt_despesa, [], data_return]
+
+
+# Add/Remove categoria receita
+@app.callback(
+    [
+        Output("category-div-add-receita", "children"),
+        Output("category-div-add-receita", "style"),
+        Output("select_receita", "options"),
+        Output('checklist-selected-style-receita', 'options'),
+        Output('checklist-selected-style-receita', 'value'),
+        Output('stored-cat-receitas', 'data')
+    ],
+
+    [
+        Input("add-category-receita", "n_clicks"),
+        Input("remove-category-receita", 'n_clicks')
+    ],
+
+    [
+        State("input-add-receita", "value"),
+        State('checklist-selected-style-receita', 'value'),
+        State('stored-cat-receitas', 'data')
+    ]
+)
+def add_category(n, n2, txt, check_delete, data):
+    cat_receita = list(data["Categoria"].values())
+
+    txt1 = []
+    style1 = {}
+
+    if n:
+        if txt == "" or txt == None:
+            txt1 = "O campo de texto não pode estar vazio para o registro de uma nova categoria."
+            style1 = {'color': 'red'}
+
+    if n and not(txt == "" or txt == None):
+        cat_receita = cat_receita + [txt] if txt not in cat_receita else cat_receita
+        txt1 = f'A categoria {txt} foi adicionada com sucesso!'
+        style1 = {'color': 'green'}
+    
+    if n2:
+        if check_delete == []:
+            pass
+        else:
+            cat_receita = [i for i in cat_receita if i not in check_delete]  
+    
+    opt_receita = [{"label": i, "value": i} for i in cat_receita]
+    df_cat_receita = pd.DataFrame(cat_receita, columns=['Categoria'])
+    df_cat_receita.to_csv("df_cat_receita.csv")
+    data_return = df_cat_receita.to_dict()
+
+    return [txt1, style1, opt_receita, opt_receita, [], data_return]
